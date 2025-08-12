d30cbda5aca96870aea982cf50c7d74367ce672d
r12053 <rahul1200533@gmail.com>
2025-08-12 22:21:24 +0530

    another update

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -9,7 +9,7 @@ export default function Login() {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("/auth/login", {
         email,
         password,
       });
