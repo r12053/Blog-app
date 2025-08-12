f09a81a0416364df6b3d3cee1ea5d7b014928d51
r12053 <rahul1200533@gmail.com>
2025-08-12 22:09:18 +0530

    update

--- frontend/src/api/axiosClient.ts
+++ frontend/src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000/api", // Change to your backend URL
+  baseURL: "https://blog-app-cial.onrender.com/api", // Change to your backend URL
   headers: {
     "Content-Type": "application/json",
   },
